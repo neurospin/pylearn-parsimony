neurospin/pylearn-parsimony

commit 854f50c98159417f630064ca9f73508399fdbe95

ENH: Updated FastSparseSVD and renamed to RankOneSparseSVD. Significant speedup with newer versions of SciPy.

parsimony/algorithms/nipals.py
@@ -41,8 +41,7 @@ try:
 except:
     has_svds = False
 
-
-__all__ = ["RankOneSVD", "FastSparseSVD", "FastSVDProduct", "PLSR"]
+__all__ = ["RankOneSVD", "RankOneSparseSVD", "FastSVDProduct", "PLSR"]
 
 # TODO: Add information about the runs.
 
@@ -65,7 +64,7 @@ class RankOneSVD(bases.ImplicitAlgorithm,
     eps : Positive float. The tolerance used by the stopping criterion.
 
     max_iter : Non-negative integer. Maximum allowed number of iterations.
-            Default is 100.
+            Default is consts.MAX_ITER.
 
     start_vector : BaseStartVector. A start vector generator. Default is to use
             a random start vector.
@@ -118,7 +117,7 @@ class RankOneSVD(bases.ImplicitAlgorithm,
                      utils.Info.func_val]
 
     def __init__(self, eps=consts.TOLERANCE,
-                 max_iter=100, min_iter=1, info=[]):
+                 max_iter=consts.MAX_ITER, min_iter=1, info=[]):
 
         super(RankOneSVD, self).__init__(info=info)
         self.max_iter = max_iter
@@ -200,85 +199,198 @@ class RankOneSVD(bases.ImplicitAlgorithm,
 
         return utils.direct_vector(v)
 
-
 FastSVD = RankOneSVD
 
 
-class FastSparseSVD(bases.ImplicitAlgorithm):
+class RankOneSparseSVD(bases.ImplicitAlgorithm,
+                       bases.InformationAlgorithm):
+    """A kernel rank-one SVD implementation for sparse CSR matrices.
 
-    def run(self, X, max_iter=100, start_vector=None):
-        """A kernel SVD implementation for sparse CSR matrices.
+    The rank-one SVD corresponds to the following optimization problem:
 
-        This is usually faster than np.linalg.svd when density < 20% and when
-        M << N or N << M (at least one order of magnitude). When M = N >= 10000
-        it is faster when the density < 1% and always faster regardless of
-        density when M = N < 10000.
+        max. ||Xv||_2 = sigma_max(X)
+        s.t. ||v||_2 = 1,
 
-        These are ballpark estimates that may differ on your computer.
+    where ||.||_2 is the 2-norm.
+
+    This is usually faster than np.linalg.svd when density < 20% and when
+    M << N or N << M (at least one order of magnitude). When M = N >= 10000
+    it is faster when the density < 1% and always faster regardless of
+    density when M = N < 10000. These are ballpark estimates that may differ on
+    your computer.
+
+    This method is faster than np.linalg.svd.
+
+    Parameters
+    ----------
+    eps : Positive float. The tolerance used by the stopping criterion.
+
+    max_iter : Non-negative integer. Maximum allowed number of iterations.
+            Default is consts.MAX_ITER.
+
+    start_vector : BaseStartVector. A start vector generator. Default is to use
+            a random start vector.
+
+    Returns
+    -------
+    v : The right singular vector of X that corresponds to the largest singular
+            value.
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> import scipy.sparse as sparse
+    >>> from parsimony.algorithms.nipals import RankOneSparseSVD
+    >>> np.random.seed(0)
+    >>> X = np.random.rand(10, 10)
+    >>> X[X < 0.1] = 0.0
+    >>> X = sparse.csr_matrix(X)
+    >>> fast_sparse_svd = RankOneSparseSVD(max_iter=1000)
+    >>> fast_sparse_svd.run(X)
+    array([[ 0.35668503],
+           [ 0.36118301],
+           [ 0.35219541],
+           [ 0.34784089],
+           [ 0.19048907],
+           [ 0.23453575],
+           [ 0.25620994],
+           [ 0.28843988],
+           [ 0.42695407],
+           [ 0.27361241]])
+    >>>
+    >>> np.random.seed(0)
+    >>> X = np.random.rand(100, 150)
+    >>> X[X < 0.5] = 0.0
+    >>> X = sparse.csr_matrix(X)
+    >>> fast_svd = RankOneSparseSVD()
+    >>> v = fast_svd.run(X)
+    >>> us = np.linalg.norm(X.dot(v))
+    >>> s = np.linalg.svd(X.todense(), full_matrices=False, compute_uv=False)
+    >>> abs(np.sum(us ** 2.0) - np.max(s) ** 2.0) < 5e-12
+    True
+    >>>
+    >>> np.random.seed(0)
+    >>> X = np.random.rand(100, 50)
+    >>> X[X < 0.9] = 0.0
+    >>> X = sparse.csr_matrix(X)
+    >>> fast_svd = RankOneSparseSVD()
+    >>> v = fast_svd.run(X)
+    >>> us = np.linalg.norm(X.dot(v))
+    >>> s = np.linalg.svd(X.todense(), full_matrices=False, compute_uv=False)
+    >>> abs(np.sum(us ** 2.0) - np.max(s) ** 2.0) < 5e-12
+    True
+    """
+    INFO_PROVIDED = [utils.Info.ok,
+                     utils.Info.time,
+                     utils.Info.func_val,
+                     utils.Info.converged]
+
+    def __init__(self, eps=consts.TOLERANCE,
+                 max_iter=consts.MAX_ITER, min_iter=1, info=[]):
+
+        super(RankOneSparseSVD, self).__init__(info=info)
+        self.max_iter = max_iter
+        self.min_iter = min_iter
+        self.eps = eps
+
+    def run(self, X, start_vector=None):
+        """Find the right-singular vector of the given sparse matrix.
 
         Parameters
         ----------
-        X : Numpy array. The matrix to decompose.
-
-        max_iter : Integer. Maximum allowed number of iterations.
+        X : Scipy sparse array. The sparse matrix to decompose.
 
         start_vector : BaseStartVector. A start vector generator. Default is
                 to use a random start vector.
-
-        Returns
-        -------
-        v : Numpy array. The right singular vector.
-
-        Example
-        -------
-        >>> import numpy as np
-        >>> from parsimony.algorithms.nipals import FastSparseSVD
-        >>> np.random.seed(0)
-        >>> X = np.random.random((10,10))
-        >>> fast_sparse_svd = FastSparseSVD()
-        >>> fast_sparse_svd.run(X)
-        array([[ 0.3522974 ],
-               [ 0.35647707],
-               [ 0.35190103],
-               [ 0.34715338],
-               [ 0.19594198],
-               [ 0.24103104],
-               [ 0.25578904],
-               [ 0.29501092],
-               [ 0.42311297],
-               [ 0.27656382]])
         """
-        if start_vector is None:
-            start_vector = start_vectors.RandomStartVector(normalise=True)
-        M, N = X.shape
-        if M < N:
-            K = X.dot(X.T)
-            t = start_vector.get_vector(X.shape[0])
-            for it in xrange(max_iter):
-                t_ = t
-                t = K.dot(t_)
-                t *= 1.0 / np.sqrt(np.sum(t ** 2.0))
+        if self.info_requested(utils.Info.ok):
+            self.info_set(utils.Info.ok, False)
 
-                a = float(np.sqrt(np.sum((t_ - t) ** 2.0)))
-                if a < consts.TOLERANCE:
-                    break
+        if self.info_requested(utils.Info.time):
+            _t = utils.time()
 
-            v = X.T.dot(t)
-            v *= 1.0 / np.sqrt(np.sum(v ** 2.0))
+        if self.info_requested(utils.Info.converged):
+            self.info_set(utils.Info.converged, False)
 
-        else:
-            K = X.T.dot(X)
-            v = start_vector.get_vector(X.shape[1])
-            for it in xrange(max_iter):
-                v_ = v
-                v = K.dot(v_)
-                v *= 1.0 / np.sqrt(np.sum(v ** 2.0))
+        arpack_failed = False
+        if has_svds:
+            if start_vector is not None:
+                v0 = start_vector.get_vector(np.min(X.shape))
+            else:
+                v0 = None
 
-                a = float(np.sqrt(np.sum((v_ - v) ** 2.0)))
-                if a < consts.TOLERANCE:
-                    break
+            from scipy.sparse.linalg import ArpackNoConvergence
 
-        return v
+            try:
+
+                [_, _, v] = sparse_linalg.svds(X, k=1, v0=v0,
+                                               tol=self.eps,
+                                               maxiter=self.max_iter,
+                                               return_singular_vectors=True)
+                v = v.T
+
+                if self.info_requested(utils.Info.converged):
+                    self.info_set(utils.Info.converged, True)
+
+            except ArpackNoConvergence:
+
+                arpack_failed = True
+
+        if not has_svds or arpack_failed:
+
+            if start_vector is None:
+                start_vector = start_vectors.RandomStartVector(normalise=True)
+
+            M, N = X.shape
+
+            if M < N:
+
+                K = X.dot(X.T)
+                t = start_vector.get_vector(X.shape[0])
+                for it in xrange(self.max_iter):
+                    t_ = t
+                    t = K.dot(t_)
+                    t *= 1.0 / maths.norm(t ** 2.0)
+
+                    crit = float(maths.norm(t_ - t)) / float(maths.norm(t))
+                    if crit < consts.TOLERANCE:
+
+                        if self.info_requested(utils.Info.converged):
+                            self.info_set(utils.Info.converged, True)
+
+                        break
+
+                v = X.T.dot(t)
+                v *= 1.0 / maths.norm(v ** 2.0)
+
+            else:
+
+                K = X.T.dot(X)
+                v = start_vector.get_vector(X.shape[1])
+                for it in xrange(self.max_iter):
+                    v_ = v
+                    v = K.dot(v_)
+                    v *= 1.0 / maths.norm(v ** 2.0)
+
+                    crit = float(maths.norm(v_ - v)) / float(maths.norm(v))
+                    if crit < consts.TOLERANCE:
+
+                        if self.info_requested(utils.Info.converged):
+                            self.info_set(utils.Info.converged, True)
+
+                        break
+
+        if self.info_requested(utils.Info.time):
+            self.info_set(utils.Info.time, utils.time() - _t)
+        if self.info_requested(utils.Info.func_val):
+            _f = maths.norm(X.dot(v))  # Largest singular value.
+            self.info_set(utils.Info.func_val, _f)
+        if self.info_requested(utils.Info.ok):
+            self.info_set(utils.Info.ok, True)
+
+        return utils.direct_vector(v)
+
+FastSparseSVD = RankOneSparseSVD
 
 
 class FastSVDProduct(bases.ImplicitAlgorithm):

parsimony/algorithms/proximal.py
@@ -633,7 +633,7 @@ class StaticCONESTA(bases.ExplicitAlgorithm,
     >>> beta1 = static_conesta.run(function, np.zeros((50, 1)))
     >>> beta2 = np.dot(np.linalg.pinv(X), y)
     >>> round(np.linalg.norm(beta1 - beta2), 13)
-    0.966290704311
+    0.9662907060983
     """
     INTERFACES = [properties.NesterovFunction,
                   properties.StepSize,

parsimony/estimators.py
@@ -744,21 +744,21 @@ class LinearRegressionL1L2TV(RegressionEstimator):
     ...                     mean=False)
     >>> res = lr.fit(X, y)
     >>> round(lr.score(X, y), 13)
-    0.0683583406798
+    0.0683615474525
     >>>
     >>> lr = estimators.LinearRegressionL1L2TV(l1, l2, tv, A,
     ...                                algorithm=proximal.FISTA(max_iter=1000),
     ...                                mean=False)
     >>> lr = lr.fit(X, y)
     >>> round(lr.score(X, y), 13)
-    1.5817577127184
+    1.3549228767247
     >>>
     >>> lr = estimators.LinearRegressionL1L2TV(l1, l2, tv, A,
     ...                                 algorithm=proximal.ISTA(max_iter=1000),
     ...                                 mean=False)
     >>> lr = lr.fit(X, y)
     >>> round(lr.score(X, y), 14)
-    2.07583068899674
+    1.92290672778289
     >>>
     >>> import parsimony.functions.nesterov.l1tv as l1tv
     >>> np.random.seed(1337)

parsimony/functions/nesterov/grouptv.py
@@ -158,11 +158,11 @@ class GroupTotalVariation(properties.NesterovFunction,
         # should allow dense matrices as well.
         if self._lambda_max is None:
 
-            from parsimony.algorithms.nipals import FastSparseSVD
+            from parsimony.algorithms.nipals import RankOneSparseSVD
 
             A = sparse.vstack(self.A())
             # TODO: Add max_iter here!
-            v = FastSparseSVD().run(A)  # , max_iter=max_iter)
+            v = RankOneSparseSVD().run(A)  # , max_iter=max_iter)
             us = A.dot(v)
             self._lambda_max = np.sum(us ** 2.0)
 

parsimony/functions/nesterov/l1tv.py
@@ -155,11 +155,11 @@ class L1TV(properties.NesterovFunction,
 
         elif self._lambda_max is None:
 
-            from parsimony.algorithms.nipals import FastSparseSVD
+            from parsimony.algorithms.nipals import RankOneSparseSVD
 
             A = sparse.vstack(self.A()[1:])
             # TODO: Add max_iter here!!
-            v = FastSparseSVD().run(A)  # , max_iter=max_iter)
+            v = RankOneSparseSVD().run(A)  # , max_iter=max_iter)
             us = A.dot(v)
             self._lambda_max = np.sum(us ** 2.0) + self.l ** 2.0
 

parsimony/functions/nesterov/tv.py
@@ -170,11 +170,11 @@ class TotalVariation(properties.NesterovFunction,
 
         elif self._lambda_max is None:
 
-            from parsimony.algorithms.nipals import FastSparseSVD
+            from parsimony.algorithms.nipals import RankOneSparseSVD
 
             A = sparse.vstack(self.A())
             # TODO: Add max_iter here!
-            v = FastSparseSVD().run(A)  # , max_iter=max_iter)
+            v = RankOneSparseSVD().run(A)  # , max_iter=max_iter)
             us = A.dot(v)
             self._lambda_max = np.sum(us ** 2.0)
 

parsimony/functions/properties.py
@@ -807,11 +807,11 @@ class NesterovFunction(Gradient,
         # should allow dense matrices as well.
         if self._lambda_max is None:
 
-            from parsimony.algorithms.nipals import FastSparseSVD
+            from parsimony.algorithms.nipals import RankOneSparseSVD
 
             A = sparse.vstack(self.A())
             # TODO: Add max_iter here!
-            v = FastSparseSVD().run(A)  # , max_iter=max_iter)
+            v = RankOneSparseSVD().run(A)  # , max_iter=max_iter)
             us = A.dot(v)
             self._lambda_max = np.sum(us ** 2.0)
 
@@ -878,12 +878,12 @@ class NesterovFunction(Gradient,
 
             def step(self, x, index=0):
                 if self._step is None:
-                    from parsimony.algorithms.nipals import FastSparseSVD
+                    from parsimony.algorithms.nipals import RankOneSparseSVD
 
                     # TODO: Avoid stacking here.
                     A = sparse.vstack(self.A)
                     # TODO: Add max_iter here!
-                    v = FastSparseSVD().run(A)  # , max_iter=max_iter)
+                    v = RankOneSparseSVD().run(A)  # , max_iter=max_iter)
                     us = A.dot(v)
                     l = np.sum(us ** 2.0)
 

commit 64b59bf60f1e3136cde81988ef40661ea8c7dff7

BUG: A bug was introduced in commit 854f50c98159417f630064ca9f73508399fdbe95. Went back to the new algorithm until we can investigate.

parsimony/algorithms/nipals.py
@@ -312,31 +312,31 @@ class RankOneSparseSVD(bases.ImplicitAlgorithm,
         if self.info_requested(utils.Info.converged):
             self.info_set(utils.Info.converged, False)
 
-        arpack_failed = False
-        if has_svds:
-            if start_vector is not None:
-                v0 = start_vector.get_vector(np.min(X.shape))
-            else:
-                v0 = None
+#        arpack_failed = False
+#        if has_svds:
+#            if start_vector is not None:
+#                v0 = start_vector.get_vector(np.min(X.shape))
+#            else:
+#                v0 = None
+#
+#            from scipy.sparse.linalg import ArpackNoConvergence
+#
+#            try:
+#
+#                [_, _, v] = sparse_linalg.svds(X, k=1, v0=v0,
+#                                               tol=self.eps,
+#                                               maxiter=self.max_iter,
+#                                               return_singular_vectors=True)
+#                v = v.T
+#
+#                if self.info_requested(utils.Info.converged):
+#                    self.info_set(utils.Info.converged, True)
+#
+#            except ArpackNoConvergence:
+#
+#                arpack_failed = True
 
-            from scipy.sparse.linalg import ArpackNoConvergence
-
-            try:
-
-                [_, _, v] = sparse_linalg.svds(X, k=1, v0=v0,
-                                               tol=self.eps,
-                                               maxiter=self.max_iter,
-                                               return_singular_vectors=True)
-                v = v.T
-
-                if self.info_requested(utils.Info.converged):
-                    self.info_set(utils.Info.converged, True)
-
-            except ArpackNoConvergence:
-
-                arpack_failed = True
-
-        if not has_svds or arpack_failed:
+        if True:  # not has_svds or arpack_failed:
 
             if start_vector is None:
                 start_vector = start_vectors.RandomStartVector(normalise=True)
@@ -350,7 +350,7 @@ class RankOneSparseSVD(bases.ImplicitAlgorithm,
                 for it in xrange(self.max_iter):
                     t_ = t
                     t = K.dot(t_)
-                    t *= 1.0 / maths.norm(t ** 2.0)
+                    t *= 1.0 / maths.norm(t)
 
                     crit = float(maths.norm(t_ - t)) / float(maths.norm(t))
                     if crit < consts.TOLERANCE:
@@ -361,7 +361,7 @@ class RankOneSparseSVD(bases.ImplicitAlgorithm,
                         break
 
                 v = X.T.dot(t)
-                v *= 1.0 / maths.norm(v ** 2.0)
+                v *= 1.0 / maths.norm(v)
 
             else:
 
@@ -370,7 +370,7 @@ class RankOneSparseSVD(bases.ImplicitAlgorithm,
                 for it in xrange(self.max_iter):
                     v_ = v
                     v = K.dot(v_)
-                    v *= 1.0 / maths.norm(v ** 2.0)
+                    v *= 1.0 / maths.norm(v)
 
                     crit = float(maths.norm(v_ - v)) / float(maths.norm(v))
                     if crit < consts.TOLERANCE:

parsimony/algorithms/proximal.py
@@ -633,7 +633,7 @@ class StaticCONESTA(bases.ExplicitAlgorithm,
     >>> beta1 = static_conesta.run(function, np.zeros((50, 1)))
     >>> beta2 = np.dot(np.linalg.pinv(X), y)
     >>> round(np.linalg.norm(beta1 - beta2), 13)
-    0.9662907060983
+    0.9662907060986
     """
     INTERFACES = [properties.NesterovFunction,
                   properties.StepSize,

parsimony/estimators.py
@@ -744,21 +744,21 @@ class LinearRegressionL1L2TV(RegressionEstimator):
     ...                     mean=False)
     >>> res = lr.fit(X, y)
     >>> round(lr.score(X, y), 13)
-    0.0683615474525
+    0.0683583406798
     >>>
     >>> lr = estimators.LinearRegressionL1L2TV(l1, l2, tv, A,
     ...                                algorithm=proximal.FISTA(max_iter=1000),
     ...                                mean=False)
     >>> lr = lr.fit(X, y)
     >>> round(lr.score(X, y), 13)
-    1.3549228767247
+    1.5817577127184
     >>>
     >>> lr = estimators.LinearRegressionL1L2TV(l1, l2, tv, A,
     ...                                 algorithm=proximal.ISTA(max_iter=1000),
     ...                                 mean=False)
     >>> lr = lr.fit(X, y)
     >>> round(lr.score(X, y), 14)
-    1.92290672778289
+    2.07583068899674
     >>>
     >>> import parsimony.functions.nesterov.l1tv as l1tv
     >>> np.random.seed(1337)