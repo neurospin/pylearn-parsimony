e8740b364e81a9057596c638964085847c945baa
duchesnay <edouard.duchesnay@gmail.com>
2019-06-03 09:58:07 +0200

    ENH: add PCA-TV

---
+++ parsimony/decomposition/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 11:20:19 CEST 2019
+
+Copyright (c) 2013-2014, CEA/DRF/Joliot/Neurospin. All rights reserved.
+
+@author:  Edouard Duchesnay
+@email:   edouard.duchesnay@cea.fr
+@license: BSD 3-clause.
+"""
+
+from .pca_tv import PCAL1L2TV
+
+__all__ = ["PCAL1L2TV"]
